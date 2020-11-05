78306b619a8747448d1dae48ca700f90bc53b9ff
Mayank Rathi <72435687+MayankDev-11@users.noreply.github.com>
2020-11-05 10:49:32 +0530

    Automate WhatsApp Message

---
+++ main.py
@@ -0,0 +1,7 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("Your Phone Number","Your Message",21,26)
+
+# Your Phone number --> example -> +91 8123498456
+# Your Message --> example -> "Hi there"
+# 21,26 --> It is the time, the time should be given in 24 hour clock and the format should be like -> time,minutes
+
