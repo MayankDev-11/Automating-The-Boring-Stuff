6cc31328ca2b4a2c9d70422bbfcffcf885884f4c
Mayank Rathi <72435687+MayankDev-11@users.noreply.github.com>
2020-11-16 16:55:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pywhatkit
+import pywhatkit #pip install pywhatkit
 pywhatkit.sendwhatmsg("Your Phone Number","Your Message",21,26)
 
 # Your Phone number --> example -> +91 8123498456
 # Your Message --> example -> "Hi there"
 # 21,26 --> It is the time, the time should be given in 24 hour clock and the format should be like -> time,minutes
-
